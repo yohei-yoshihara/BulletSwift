c15d29a73323ff81cada5d5dd804d8ea7d9e7493
Yohei Yoshihara <yy2.0101@gmail.com>
2018-09-24 20:44:26 +0900

    Initial Commit

---
+++ BulletSwift/BulletSwift.h
@@ -0,0 +1,13 @@
+// Copyright (c) Yohei Yoshihara. All rights reserved. 
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BulletSwift.
+FOUNDATION_EXPORT double BulletSwiftVersionNumber;
+
+//! Project version string for BulletSwift.
+FOUNDATION_EXPORT const unsigned char BulletSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BulletSwift/PublicHeader.h>
+
+
